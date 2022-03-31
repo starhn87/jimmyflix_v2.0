714b201d300f9dc5e36cee13187a50b6bc9e017e
starhn87 <starhn87@gmail.com>
2022-03-31 15:24:22 +0900

    HOTFIX: git 충돌 해결

--- src/Components/Info.tsx
+++ src/Components/Info.tsx
@@ -381,18 +381,6 @@ function Info() {
                                   season.poster_path
                                     ? `https://image.tmdb.org/t/p/original${season.poster_path}`
                                     : defaultPosterImg
-<<<<<<< HEAD
-=======
-                                }
-                                {
-                                    result.genres &&
-                                    <>
-                                        <Divider>•</Divider>
-                                        <Item>
-                                            {result.genres.map((genre: { name: string }, index: number) => index === result.genres.length - 1 ? genre.name : `${genre.name} / `)}
-                                        </Item>
-                                    </>
->>>>>>> bcddabf3782d8b0a1f83373876eb2402b19c99f2
                                 }
                                 alt={season.name}
                               />
