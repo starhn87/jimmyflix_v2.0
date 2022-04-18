df66e31c1452c03140cef4aae184f7d46a5aedf6
starhn87 <starhn87@gmail.com>
2022-04-18 10:20:23 +0900

    HOtFIX: webpack png 경로 수정

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,7 +17,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'images/[name].[contenthash].[ext]',
+              name: 'assets/images/[name].[ext]',
             },
           },
         ],
