95ae62e5621f17348a555ea5947a3f8a1199f9d4
starhn87 <starhn87@gmail.com>
2022-04-18 11:34:48 +0900

    HOTFIX: outputPath, name 설정

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,8 +17,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
+              name: 'images/[name].[ext]',
               esModule: false,
             },
           },
